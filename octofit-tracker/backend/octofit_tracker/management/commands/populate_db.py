4579a5c21dfe39836989c1fed01fa5c93d34c60a
P Sai Bhagyesh Reddy <126335256+Bhagyeshredde@users.noreply.github.com>
2026-03-13 06:11:28 +0000

    Add OctofitTracker app configuration and populate_db command with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel', members=['Iron Man', 'Captain America', 'Thor'])
+        dc = Team.objects.create(name='DC', members=['Superman', 'Batman', 'Wonder Woman'])
+
+        # Create users
+        User.objects.create(email='ironman@marvel.com', name='Iron Man', team='Marvel', is_active=True)
+        User.objects.create(email='cap@marvel.com', name='Captain America', team='Marvel', is_active=True)
+        User.objects.create(email='thor@marvel.com', name='Thor', team='Marvel', is_active=True)
+        User.objects.create(email='superman@dc.com', name='Superman', team='DC', is_active=True)
+        User.objects.create(email='batman@dc.com', name='Batman', team='DC', is_active=True)
+        User.objects.create(email='wonderwoman@dc.com', name='Wonder Woman', team='DC', is_active=True)
+
+        # Create activities
+        Activity.objects.create(user='Iron Man', type='Running', duration=30, date='2026-03-13')
+        Activity.objects.create(user='Captain America', type='Cycling', duration=45, date='2026-03-13')
+        Activity.objects.create(user='Thor', type='Swimming', duration=60, date='2026-03-13')
+        Activity.objects.create(user='Superman', type='Running', duration=50, date='2026-03-13')
+        Activity.objects.create(user='Batman', type='Cycling', duration=40, date='2026-03-13')
+        Activity.objects.create(user='Wonder Woman', type='Swimming', duration=55, date='2026-03-13')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=135)
+        Leaderboard.objects.create(team='DC', points=145)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', description='Do pushups', difficulty='Easy')
+        Workout.objects.create(name='Pullups', description='Do pullups', difficulty='Medium')
+        Workout.objects.create(name='Squats', description='Do squats', difficulty='Hard')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data'))
